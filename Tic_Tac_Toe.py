780c8c5f354f17c6bc8b6dcbb8959509cf1a2346
Juan Felipe Rendon Herrera <felipe28769@gmail.com>
2024-11-18 21:02:13 -0500

    Doc Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,6 +1,7 @@
 import random
 import time
 
+# Crea el tablero inicial con 'X' en el centro (posición inicial de la máquina)
 def crear_tablero():
     board = [
         [1, 2, 3],
@@ -9,6 +10,7 @@ def crear_tablero():
     ]
     return board
 
+# Muestra el estado actual del tablero en la consola
 def estado_del_tablero(board):
     print(f'''                                              +-------+-------+-------+
                                               |       |       |       |
@@ -24,6 +26,7 @@ def estado_del_tablero(board):
                                               |       |       |       |
                                               +-------+-------+-------+                                    ''')
     
+# Maneja y procesa el movimiento del jugador, asegurando que sea válido
 def movimiento_usuario(board, name = "Player"):
     while True:
         try:
@@ -48,21 +51,26 @@ def movimiento_usuario(board, name = "Player"):
             print("Por favor ingrese un numero valido")
             continue
 
-
+# Retorna una lista de las posiciones disponibles en el tablero
 def campos_libres(board):
     field = [(columna,fila) for columna in range(3) for fila in range(3) if isinstance(board[columna][fila], int)]
     return field if field else False
 
+# Verifica si hay un ganador en el tablero actual
 def verificar_tablero(board, sign):
+    # Revisa filas
     for posibilidades in range(3): 
         if board[posibilidades][0] == board[posibilidades][1] == board[posibilidades][2] == sign:   
             return True
+        # Revisa columnas
         elif board[0][posibilidades] == board[1][posibilidades] == board[2][posibilidades] == sign: 
             return True
+    # Revisa diagonales
     if board[0][0] == board[1][1] == board[2][2] == sign or board[0][2] == board[1][1] == board[2][0] == sign:
         return True
     return False
 
+# Determina y retorna el resultado final del juego
 def resultado(board, sign):
     if verificar_tablero(board, 'X'):
         return "La maquina gana -> X"
@@ -71,6 +79,7 @@ def resultado(board, sign):
     elif not verificar_tablero(board, sign):
         return "Empate"
 
+# Procesa el movimiento de la máquina de manera aleatoria
 def movimiento_maquina(board):
         casillas_libres = campos_libres(board)
 
@@ -83,6 +92,7 @@ def movimiento_maquina(board):
             time.sleep(1)
             return True
         
+# Muestra las instrucciones del juego al usuario
 def instrucciones():
     bienvenida = ["Bienvenido al Tutorial de Tic Tac Toe",
         "1. El tablero tiene 9 casillas numeradas del 1 al 9",
@@ -119,6 +129,7 @@ def instrucciones():
     ''')
     time.sleep(3)
 
+# Bucle principal del juego
 while True:
     print("Bienvenido al juego de Tic Tac Toe")
     print("1. Iniciar juego")
@@ -126,10 +137,10 @@ while True:
     print("3. Salir del juego")
     
     try:
-    
         opcion = int(input("Elija una opción: "))
 
         match opcion:
+                # Opción para iniciar el juego
                 case 1:
                     player_name = input("Ingresa tu nombre: ")
                         
@@ -137,30 +148,36 @@ while True:
                     estado_del_tablero(tablero)
                     
                     while True:
-                        
+                        # Turno del jugador
                         movimiento_usuario(tablero, player_name)
                         estado_del_tablero(tablero)
 
+                        # Verifica si el jugador ganó
                         if verificar_tablero(tablero, 'O'):
                             print(resultado(tablero, player_name))
                             break
                         
+                        # Turno de la máquina
                         movimiento_maquina(tablero)
                         estado_del_tablero(tablero)
                         
+                        # Verifica si la máquina ganó
                         if verificar_tablero(tablero, 'X'):
                             print(resultado(tablero, player_name))
                             break
                         
+                        # Verifica si hay empate
                         if not verificar_tablero(tablero, 'X') and not verificar_tablero(tablero, 'O') and not campos_libres(tablero):
                             print("Empate")
                             break
         
+                # Opción para mostrar las instrucciones
                 case 2:
                     print("\n")
                     instrucciones()
                     print("\n")
 
+                # Opción para salir del juego
                 case 3:
                     print("\n","¡Gracias por jugar!")
                     break
